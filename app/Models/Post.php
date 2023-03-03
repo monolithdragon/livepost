5f3797ec0fc8098e967290db5b5ff48a80f77446
monolithdragon <joo.lajos76@gmail.com>
2023-03-03 15:16:32 +0100

    Updated Models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'body' => 'array'
+    ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'post_id');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'post_user', 'post_id', 'user_id');
+    }
 }
